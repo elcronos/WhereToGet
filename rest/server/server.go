3d4ac36546215988d264bbbc31a5e1e2eee1cd1c
Camilo Pestana <capcarde@hotmail.com>
2017-08-25 13:35:24 +0800

    Email

--- rest/server/server.go
+++ rest/server/server.go
@@ -38,7 +38,7 @@ func init(){
 	routes.GET("/services/:id", handler.GetService)
 	routes.GET("/products", handler.GetAllProducts)
 	routes.GET("/products/:id", handler.GetProduct)
-	routes.POST("/email", handler.SendEmail)
+	routes.POST("/emagil", handler.SendEmail)
 	routes.OPTIONS("/email", handler.OptionsResponse)
 
 	//Routes Handler
